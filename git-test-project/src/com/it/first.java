96de8b81eb547b1c081bcf89a2547d3c7d63d750
WeFat <641847598@qq.com>
2020-07-16 05:38:14 +0800

    first git COMMIT

---
+++ git-test-project/src/com/it/first.java
@@ -0,0 +1,10 @@
+package com.it;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("hell");
+        System.out.println("hell");
+        System.out.println("hell");
+
+    }
+}
